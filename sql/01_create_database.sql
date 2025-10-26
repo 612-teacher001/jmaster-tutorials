12eb51db20cd7fa4b2d2e17edc278fba46bf4011
villageworks <villageworks@gmail.com>
2025-10-27 00:03:39 +0900

    T3.01（ruby）商品管理：データベース初期化Tascによる自動化

---
+++ sql/01_create_database.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS jbasicdaodb;
+-- DROP ROLE IF EXISTS student;
+-- CREATE ROLE student WITH PASSWORD 'himitu' LOGIN;
+CREATE DATABASE jbasicdaodb OWNER student ENCODING 'UTF8';
